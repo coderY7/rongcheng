e7abc8a04a0ce9262d6417654e73fa99528ac56a
coderyh <2368009927@qq.com>
2022-12-10 14:17:26 +0800

    git

--- App.vue
+++ App.vue
@@ -10,6 +10,32 @@
       uni.setStorageSync("rkdh", '')
       uni.setStorageSync("thdh", '')
       uni.setStorageSync("pddh", '')
+
+      const updateManager = uni.getUpdateManager();
+
+      updateManager.onCheckForUpdate(function (res) {
+        // 请求完新版本信息的回调
+        console.log(res.hasUpdate);
+      });
+
+      updateManager.onUpdateReady(function (res) {
+        uni.showModal({
+          title: '更新提示',
+          content: '新版本已经准备好，是否重启应用？',
+          success(res) {
+            if (res.confirm) {
+              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+              updateManager.applyUpdate();
+            }
+          }
+        });
+
+      });
+
+      updateManager.onUpdateFailed(function (res) {
+        // 新的版本下载失败
+      });
+
     },
 		onShow: function(options) {
       let pdlxlist=[
@@ -53,29 +79,7 @@
 
 
 
-      const updateManager = wx.getUpdateManager()
 
-      updateManager.onCheckForUpdate(function (res) {
-        // 请求完新版本信息的回调
-        console.log(res.hasUpdate)
-      })
-
-      updateManager.onUpdateReady(function () {
-        wx.showModal({
-          title: '更新提示',
-          content: '新版本已经准备好，是否重启应用？',
-          success: function (res) {
-            if (res.confirm) {
-              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
-              updateManager.applyUpdate()
-            }
-          }
-        })
-      })
-
-      updateManager.onUpdateFailed(function () {
-        // 新版本下载失败
-      })
 
     },
 		onHide: function() {
--- unpackage/dist/dev/mp-weixin/common/main.js
+++ unpackage/dist/dev/mp-weixin/common/main.js
@@ -107,6 +107,32 @@ __webpack_require__.r(__webpack_exports__);
     uni.setStorageSync("rkdh", '');
     uni.setStorageSync("thdh", '');
     uni.setStorageSync("pddh", '');
+
+    var updateManager = uni.getUpdateManager();
+
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate);
+    });
+
+    updateManager.onUpdateReady(function (res) {
+      uni.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function success(res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate();
+          }
+        } });
+
+
+    });
+
+    updateManager.onUpdateFailed(function (res) {
+      // 新的版本下载失败
+    });
+
   },
   onShow: function onShow(options) {
     var pdlxlist = [
@@ -150,29 +176,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-    var updateManager = wx.getUpdateManager();
 
-    updateManager.onCheckForUpdate(function (res) {
-      // 请求完新版本信息的回调
-      console.log(res.hasUpdate);
-    });
-
-    updateManager.onUpdateReady(function () {
-      wx.showModal({
-        title: '更新提示',
-        content: '新版本已经准备好，是否重启应用？',
-        success: function success(res) {
-          if (res.confirm) {
-            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
-            updateManager.applyUpdate();
-          }
-        } });
-
-    });
-
-    updateManager.onUpdateFailed(function () {
-      // 新版本下载失败
-    });
 
   },
   onHide: function onHide() {
